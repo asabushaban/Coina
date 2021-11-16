553c8c2f56c12cde42bbc3c248140fcc3fa13ea7
AJ Abushaban <amjad.abushaban@yahoo.com>
2021-11-16 09:51:39 -0600

    added a few rows to user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -7,6 +8,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
