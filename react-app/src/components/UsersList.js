9f6d075fc25d35ae585390b530ec560906144198
AJ Abushaban <amjad.abushaban@yahoo.com>
2021-11-23 14:18:56 -0600

    resized the logout button

--- react-app/src/components/UsersList.js
+++ react-app/src/components/UsersList.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function UsersList() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('/api/users/');
+      const response = await fetch("/api/users/");
       const responseData = await response.json();
       setUsers(responseData.users);
     }
     fetchData();
   }, []);
 
-  const userComponents = users.map((user) => {
+  const userComponents = users.map(user => {
     return (
       <li key={user.id}>
         <NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
